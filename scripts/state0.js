28376a9794431154d371a2842daea2e4f7313e1b
Jacques Begin <jacques.andre.begin@gmail.com>
2017-09-11 16:19:38 -0400

    Adds robot walking animation

--- scripts/state0.js
+++ scripts/state0.js
@@ -1,34 +1,58 @@
 var demo = {};
 var centerX = 1500 / 2, centerY = 1000 / 2;
-var robot, speed = 5;
+var robot, speed = 8;
 demo.state0 = function(){};
 demo.state0.prototype = {
   preload: function(){
-    game.load.image('robot', 'assets/sprites/robotwalker/robot1.png');
+    game.load.image('hallway', 'assets/backgrounds/hallway2000.png');
+    game.load.spritesheet('robot', 'assets/spritesheets/robotwalking.png', 320, 320);
 
 
   },
   create: function(){
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     game.stage.backgroundColor = "#ffffff";
     console.log("in state0");
     addChangeStateEventListeners();
+    game.world.setBounds(0, 0, 2000, 1000);
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    
+
+    var hallwayBG = game.add.sprite(0, 0, 'hallway');
     robot = game.add.sprite(centerX, centerY, 'robot');
     robot.anchor.setTo(0.5, 0.5);
+    robot.scale.setTo(0.7, 0.7);
+    game.physics.enable(robot);
+    robot.body.collideWorldBounds = true;
+    robot.animations.add('walking', [2, 3, 0, 1]);
+
+    game.camera.follow(robot);
+    game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0, 600, 1000);
+    
   },
   update: function(){
     if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+      robot.scale.setTo(0.7, 0.7);
       robot.x += speed;
-    }
-    if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+      robot.animations.play('walking', 14, true);
+    } else if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+      robot.scale.setTo(-0.7, 0.7);
       robot.x -= speed;
+      robot.animations.play('walking', 14, true);
+    } else {
+      robot.animations.stop('walking');
+      robot.frame = 3;
     }
+
     if(game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
       robot.y -= speed;
-    }
-    if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+      if (robot.y < 232) {
+        robot.y = 232;
+      }
+    } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
       robot.y += speed;
+      if (robot.y > 720) {
+        robot.y = 720;
+      }
     }
   }
 };
